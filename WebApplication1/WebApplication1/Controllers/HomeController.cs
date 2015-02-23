df97d60185b9ad528388d08a7aafd96c3cf38182
aleti568 <aletisandeep2010@gmail.com>
2015-02-22 20:15:08 -0600

    ch

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
-            
+            //
             //jbhkk
             return View();
         }
